93b0952cec9506efaab188f98ec882640a3e33e9
Sarah Herzog <235290+CodingDino@users.noreply.github.com>
2021-12-03 13:09:10 +0000

    Create and draw ball sprite, set up window, close on escape

--- Interpolation and Derivatives Project/Main.cpp
+++ Interpolation and Derivatives Project/Main.cpp
@@ -3,24 +3,60 @@
 int main()
 {
 
-	sf::RenderWindow window(sf::VideoMode(200, 200), "SFML Works!");
-	sf::CircleShape shape(100.f);
-	shape.setFillColor(sf::Color::Green);
+	// Window Setup
+	sf::RenderWindow window(sf::VideoMode::getDesktopMode(), "Interpolation Example", sf::Style::Titlebar);
 
+	// Load texture
+	sf::Texture ballTexture;
+	ballTexture.loadFromFile("Assets/Graphics/ball_bowling.png");
+
+	// Setup Sprite
+	sf::Sprite ballSprite;
+	ballSprite.setTexture(ballTexture);
+	sf::Vector2f ballPosition = sf::Vector2f(0.0f, 0.0f);
+	ballSprite.setPosition(ballPosition);
+	ballSprite.setOrigin(ballTexture.getSize().x / 2, ballTexture.getSize().y / 2);
+
+	// ---------------------------------------------------
+	// Game Loop
+	// ---------------------------------------------------
 	while (window.isOpen())
 	{
-
+		// ---------------------------------------------------
+		// Input
+		// ---------------------------------------------------
 		sf::Event event;
 		while (window.pollEvent(event))
 		{
 			if (event.type == sf::Event::Closed)
 				window.close();
+
+			// Is the event a key press event
+			if (event.type == sf::Event::KeyPressed)
+			{
+				// Is the event specifically related to the escape key
+				if (event.key.code == sf::Keyboard::Escape)
+				{
+					window.close();
+				}
+			}
 		}
 
-		window.clear();
-		window.draw(shape);
-		window.display();
 
+		// ---------------------------------------------------
+		// Update
+		// ---------------------------------------------------
+
+
+		// ---------------------------------------------------
+		// Draw
+		// ---------------------------------------------------
+		sf::Color clearColor(200,200,200);
+		window.clear(clearColor);
+
+		window.draw(ballSprite);
+
+		window.display();
 	}
 
 
